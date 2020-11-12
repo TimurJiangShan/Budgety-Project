61ce309a81ad68724eb36194640c122b3d11b44e
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-11-12 15:52:15 +1100

    adding myBind

--- MyBind.js
+++ MyBind.js
@@ -1,12 +1,24 @@
-Function.prototype.myBind = function (context, ...args1) {
+/**
+ * bind() 方法会创建一个新函数。当这个函数被调用时，bind() 的第一个参数将作为它运行时的this，
+ * 之后的一系列参数将会在传递的实参前传入作为它的参数。
+ * 1. 返回一个函数
+ * 2. 可以传入参数
+*/
 
-    if( typeof this !== 'function')
-        throw new Error("Not a function");
-
+/***************************  第一版  ********************************/
+Function.prototype.myBind1 = function(context){
     let self = this;
-    let fn = function(...args2){
-        return self.apply(this instanceof fn ? this : context, args1.concat(args2));
+    return function(){
+        return self.apply(context);
     }
-
-    
 }
+var foo1 = {
+    value: 1
+};
+
+function bar1() {
+	return this.value;
+}
+
+let bindFoo = bar1.myBind1(foo1);
+console.log(bindFoo());
